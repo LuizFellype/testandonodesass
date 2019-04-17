c91b9cf812e1cd530ecffa6320d3dae7e7907c39
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-17 00:28:55 -0300

    refactor(login): change login directory

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -3,21 +3,11 @@ import { InputText } from 'primereact/inputtext'
 import { Password } from 'primereact/password'
 import { Button } from 'primereact/button'
 import avatar from '../../assets/img/flat-avatar.png'
-import LoginWrapper from './LoginWrapper'
-import LoginRow from './LoginRow'
-import Label from './Label'
+import LoginWrapper from '../../components/auth/LoginWrapper'
+import LoginRow from '../../components/auth/LoginRow'
+import Label from '../../components/auth/Label'
 
 const LoginPage = () => (
-  // getInitialState: function() {
-  //   return {
-  //     loginID: '',
-  //     password: '',
-  //     isSubmitted: false
-  //   }
-  // },
-
-  // mixins: [History],
-
   <LoginWrapper>
     <img src={avatar} className='user-avatar' alt='Logo' />
     <h1>Faça login para continuar no sistema</h1>
@@ -37,28 +27,5 @@ const LoginPage = () => (
     </form>
   </LoginWrapper>
 )
-
-// setLoginID: function(e) {
-//   this.setState({
-//     loginID: e.target.value,
-//     loginError: ''
-//   })
-// },
-
-// setPassword: function(e) {
-//   this.setState({
-//     password: e.target.value,
-//     loginError: ''
-//   })
-// },
-
-// handleLogin: function(e) {
-//   e.preventDefault()
-//   this.props.history.pushState(null, '/')
-
-//   // this.transitionTo('dashboard');
-
-//   return false
-// }
 
 export default LoginPage
