5c7f67d48fc4de0117ac9f0a0ae88e9106a363c5
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-19 02:15:26 -0300

    feat(constants): add images list

--- src/constants.js
+++ src/constants.js
@@ -191,3 +191,26 @@ export const menuItems = [
     ]
   }
 ]
+
+export const images = [
+  {
+    source: 'assets/demo/images/sopranos/sopranos1.jpg',
+    thumbnail: 'assets/demo/images/sopranos/sopranos1_small.jpg',
+    title: 'Sopranos 1'
+  },
+  {
+    source: 'assets/demo/images/sopranos/sopranos2.jpg',
+    thumbnail: 'assets/demo/images/sopranos/sopranos2_small.jpg',
+    title: 'Sopranos 2'
+  },
+  {
+    source: 'assets/demo/images/sopranos/sopranos3.jpg',
+    thumbnail: 'assets/demo/images/sopranos/sopranos3_small.jpg',
+    title: 'Sopranos 3'
+  },
+  {
+    source: 'assets/demo/images/sopranos/sopranos4.jpg',
+    thumbnail: 'assets/demo/images/sopranos/sopranos4_small.jpg',
+    title: 'Sopranos 4'
+  }
+]
