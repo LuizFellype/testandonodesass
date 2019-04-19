a439b58824574564fe43d819de6a1a82586a7248
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-19 14:24:49 -0300

    feat(upload): add upload component

---
+++ src/components/misc-demo/Upload.js
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react'
+
+// Prime components
+import { FileUpload } from 'primereact/fileupload'
+import { Growl } from 'primereact/growl'
+
+const Upload = () => {
+  const growl = useRef(null)
+
+  const onUpload = () => {
+    growl.show({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded'
+    })
+  }
+
+  return (
+    <div className='card'>
+      <h1>Upload</h1>
+      <Growl ref={growl} />
+
+      <FileUpload
+        name='demo[]'
+        url='./upload.php'
+        onUpload={onUpload}
+        multiple
+        accept='image/*'
+        maxFileSize={1000000}
+      />
+    </div>
+  )
+}
+
+export default Upload
