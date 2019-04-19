b907bf792bb3e247d77bd18533c4b212cbf4a060
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-19 02:21:28 -0300

    refactor(overlays): change to functional component

--- src/pages/components/OverlaysDemo.js
+++ src/pages/components/OverlaysDemo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 // Custom components
 import OverlayPanel from '../../components/overlays-demo/OverlayPanel'
@@ -6,20 +6,18 @@ import Dialog from '../../components/overlays-demo/Dialog'
 import Lightbox from '../../components/overlays-demo/Lightbox'
 import Tooltip from '../../components/overlays-demo/Tooltip'
 
-export class OverlaysDemo extends Component {
-  render() {
-    return (
-      <div className='p-grid p-fluid'>
-        <div className='p-col-12 p-lg-6'>
-          <OverlayPanel />
-          <Dialog />
-        </div>
+const OverlaysDemo = () => (
+  <div className='p-grid p-fluid'>
+    <div className='p-col-12 p-lg-6'>
+      <OverlayPanel />
+      <Dialog />
+    </div>
 
-        <div className='p-col-12 p-lg-6'>
-          <Lightbox />
-          <Tooltip />
-        </div>
-      </div>
-    )
-  }
-}
+    <div className='p-col-12 p-lg-6'>
+      <Lightbox />
+      <Tooltip />
+    </div>
+  </div>
+)
+
+export default OverlaysDemo
