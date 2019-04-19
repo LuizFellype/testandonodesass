4066bd45e16f9f426d72cf97731db2302963c24c
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-19 02:16:21 -0300

    refactor(overlays): add lightbox component

--- src/pages/components/OverlaysDemo.js
+++ src/pages/components/OverlaysDemo.js
@@ -1,40 +1,12 @@
 import React, { Component } from 'react'
-import { Lightbox } from 'primereact/lightbox'
 import { InputText } from 'primereact/inputtext'
 
 // Custom components
 import OverlayPanel from '../../components/overlays-demo/OverlayPanel'
 import Dialog from '../../components/overlays-demo/Dialog'
+import Lightbox from '../../components/overlays-demo/Lightbox'
 
 export class OverlaysDemo extends Component {
-  constructor() {
-    super()
-    this.state = {
-      images: [
-        {
-          source: 'assets/demo/images/sopranos/sopranos1.jpg',
-          thumbnail: 'assets/demo/images/sopranos/sopranos1_small.jpg',
-          title: 'Sopranos 1'
-        },
-        {
-          source: 'assets/demo/images/sopranos/sopranos2.jpg',
-          thumbnail: 'assets/demo/images/sopranos/sopranos2_small.jpg',
-          title: 'Sopranos 2'
-        },
-        {
-          source: 'assets/demo/images/sopranos/sopranos3.jpg',
-          thumbnail: 'assets/demo/images/sopranos/sopranos3_small.jpg',
-          title: 'Sopranos 3'
-        },
-        {
-          source: 'assets/demo/images/sopranos/sopranos4.jpg',
-          thumbnail: 'assets/demo/images/sopranos/sopranos4_small.jpg',
-          title: 'Sopranos 4'
-        }
-      ]
-    }
-  }
-
   render() {
     return (
       <div className='p-grid p-fluid'>
@@ -42,11 +14,9 @@ export class OverlaysDemo extends Component {
           <OverlayPanel />
           <Dialog />
         </div>
+
         <div className='p-col-12 p-lg-6'>
-          <div className='card'>
-            <h1>LightBox</h1>
-            <Lightbox images={this.state.images} />
-          </div>
+          <Lightbox />
           <div className='card'>
             <h1>Tooltip</h1>
             <InputText
