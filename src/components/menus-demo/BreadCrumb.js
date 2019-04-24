35adfa1a3702ab1406280fd18783f8c1adb36ade
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-24 17:22:40 -0300

    feat(breadcrumb): add breadcrumb menu component

---
+++ src/components/menus-demo/BreadCrumb.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { breadcrumbItems, home } from '../../constants'
+
+// Prime BreadCrumb component
+import { BreadCrumb } from 'primereact/breadcrumb'
+
+const CustomBreadCrumb = () => (
+  <div className='card card-w-title'>
+    <h1>Breadcrumb</h1>
+    <BreadCrumb model={breadcrumbItems} home={home} />
+  </div>
+)
+
+export default CustomBreadCrumb
