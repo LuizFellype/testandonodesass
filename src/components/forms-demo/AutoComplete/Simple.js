4496eb2e03f533ceb18a84ee8e610088b1f48887
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-25 01:45:57 -0300

    feat(autocomplete): add simple autocomplete component

---
+++ src/components/forms-demo/AutoComplete/Simple.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import CountryService from '../../../service/CountryService'
+import Label from './Label'
+
+// Prime AutoComplete component
+import { AutoComplete } from 'primereact/autocomplete'
+
+const Simple = () => {
+  const [countriesData, setCountriesData] = useState([])
+  const [filteredCountries, setFilteredCountries] = useState([])
+  const [country, setCountry] = useState('')
+
+  useEffect(() => {
+    CountryService.getCountries(setCountriesData)
+  }, [])
+
+  const filterCountry = ({ query }) => {
+    const filteredCountries = countriesData.filter(country =>
+      country.name.toLowerCase().startsWith(query.toLowerCase())
+    )
+    setFilteredCountries(filteredCountries)
+  }
+
+  return (
+    <>
+      <Label htmlFor='acSimple' text='Simple' />
+      <div className='p-col-12' style={{ marginBottom: '10px' }}>
+        <AutoComplete
+          minLength={1}
+          placeholder='Countries'
+          id='acSimple'
+          size={30}
+          field='name'
+          suggestions={filteredCountries}
+          completeMethod={filterCountry}
+          value={country}
+          onChange={({ value }) => {
+            setCountry(value)
+            setFilteredCountries([])
+          }}
+        />
+      </div>
+    </>
+  )
+}
+
+export default Simple
