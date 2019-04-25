8507a4e1da27610f5f81ec1a388ffe0c05eebd70
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-25 02:03:29 -0300

    feat(group): add input group wrapper component

---
+++ src/components/forms-demo/InputGroups/InputGroupElementWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InputGroupElementWrapper = ({ component: Component }) => (
+  <div className='p-col-12 p-md-6'>
+    <Component />
+  </div>
+)
+
+export default InputGroupElementWrapper
