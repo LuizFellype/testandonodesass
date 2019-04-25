51591ad86160280cc8c93190574fa3e64fc6b769
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-25 02:09:08 -0300

    feat(raised): add raised buttons sample component

---
+++ src/components/forms-demo/ColoredButtons/RaisedButtons.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+// Prime Button component
+import { Button } from 'primereact/button'
+
+const RaisedButtons = () => (
+  <div className='p-col-12 p-md-4' style={{ textAlign: 'center' }}>
+    <Button label='Primary' style={{ marginBottom: '10px' }} />
+    <Button
+      label='Secondary'
+      style={{ marginBottom: '10px' }}
+      className='p-button-secondary'
+    />
+    <Button
+      label='Success'
+      style={{ marginBottom: '10px' }}
+      className='p-button-success'
+    />
+    <Button
+      label='Info'
+      style={{ marginBottom: '10px' }}
+      className='p-button-info'
+    />
+    <Button
+      label='Warning'
+      style={{ marginBottom: '10px' }}
+      className='p-button-warning'
+    />
+    <Button
+      label='Danger'
+      style={{ marginBottom: '10px' }}
+      className='p-button-danger'
+    />
+  </div>
+)
+
+export default RaisedButtons
