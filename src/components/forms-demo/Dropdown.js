c05b9334822608eed2dbd2407dd810d3dfaa4ccf
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-25 01:52:45 -0300

    feat(dropdown): add dropdown demo component

---
+++ src/components/forms-demo/Dropdown.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { cities } from '../../constants'
+
+// Prime Dropdown component
+import { Dropdown } from 'primereact/dropdown'
+
+const CustomDropdown = () => {
+  const [dropdownCity, setDropdownCity] = useState(null)
+
+  return (
+    <div className='card card-w-title'>
+      <h1>Dropdown</h1>
+      <Dropdown
+        options={cities}
+        value={dropdownCity}
+        onChange={({ value }) => setDropdownCity(value)}
+        autoWidth={false}
+      />
+    </div>
+  )
+}
+
+export default CustomDropdown
