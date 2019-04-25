4401f5429c4aabf6f23cb6e53cfae95895ae6150
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-25 17:25:35 -0300

    refactor(sample): remove duplicated components

--- src/pages/components/SampleDemo.js
+++ src/pages/components/SampleDemo.js
@@ -3,11 +3,6 @@ import React from 'react'
 // Customized SampleDemo page components
 import TopMessage from '../../components/sample-demo/TopMessage'
 import FormElements from '../../components/sample-demo/FormElements'
-import DataTable from '../../components/sample-demo/DataTable'
-import DataView from '../../components/sample-demo/DataView'
-import PickList from '../../components/sample-demo/Pick'
-import OrderList from '../../components/sample-demo/OrderList'
-import Tree from '../../components/sample-demo/Tree'
 
 const SampleDemo = () => (
   <div className='p-fluid'>
@@ -15,18 +10,6 @@ const SampleDemo = () => (
       <div className='p-col-12'>
         <TopMessage />
         <FormElements />
-        <DataTable />
-      </div>
-
-      <DataView />
-
-      <div className='p-col-12 p-lg-6'>
-        <PickList />
-        <OrderList />
-      </div>
-
-      <div className='p-col-12 p-lg-6'>
-        <Tree />
       </div>
     </div>
   </div>
