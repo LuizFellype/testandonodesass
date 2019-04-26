8ef09bc62aa761c526c75dfc8ada5734d175687a
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-26 03:17:47 -0300

    feat(topbar): add topbar settings button component

---
+++ src/containers/TopBar/Settings.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Settings = () => (
+  <button className='p-link'>
+    <span className='layout-topbar-item-text'>Settings</span>
+    <span className='layout-topbar-icon pi pi-cog' />
+  </button>
+)
+
+export default Settings
