62505b2848206cf320c5e75ae786c2fcf4f47ba6
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-26 02:14:05 -0300

    refactor(services): refactor to functional code

--- src/service/CarService.js
+++ src/service/CarService.js
@@ -1,23 +1,12 @@
 import axios from 'axios'
 
-export class CarService {
-  getCarsSmall() {
-    return axios
-      .get('assets/demo/data/cars-small.json')
-      .then(res => res.data.data)
-  }
+const getCarsSmall = () =>
+  axios.get('assets/demo/data/cars-small.json').then(res => res.data.data)
 
-  getCarsMedium() {
-    return axios
-      .get('assets/demo/data/cars-medium.json')
-      .then(res => res.data.data)
-  }
+const getCarsMedium = () =>
+  axios.get('assets/demo/data/cars-medium.json').then(res => res.data.data)
 
-  getCarsLarge() {
-    return axios
-      .get('assets/demo/data/cars-large.json')
-      .then(res => res.data.data)
-  }
-}
+const getCarsLarge = () =>
+  axios.get('assets/demo/data/cars-large.json').then(res => res.data.data)
 
-export default new CarService()
+export default { getCarsSmall, getCarsMedium, getCarsLarge }
--- src/service/CountryService.js
+++ src/service/CountryService.js
@@ -1,15 +1,6 @@
 import axios from 'axios'
 
-export class CountryService {
-  getCountries(setCountriesData) {
-    return axios
-      .get('assets/demo/data/countries.json')
-      .then(res => res.data.data)
-      .then(data => {
-        setCountriesData(data)
-        return data
-      })
-  }
-}
+const getCountries = () =>
+  axios.get('assets/demo/data/countries.json').then(res => res.data.data)
 
-export default new CountryService()
+export default { getCountries }
