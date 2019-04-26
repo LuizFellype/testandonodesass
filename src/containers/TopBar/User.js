71e3edac11ac71aeaf02034fb5570cf47f59448d
xavierleonardo <gxavier.leonardo@gmail.com>
2019-04-26 03:18:15 -0300

    feat(topbar): add topbar user button component

---
+++ src/containers/TopBar/User.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const User = () => (
+  <button className='p-link'>
+    <span className='layout-topbar-item-text'>User</span>
+    <span className='layout-topbar-icon pi pi-user' />
+  </button>
+)
+
+export default User
