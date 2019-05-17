1de77df4dd56e92cd0bddc3f989919c30111a610
xavierleonardo <gxavier.leonardo@gmail.com>
2019-05-17 18:28:57 -0300

    fix(service): fix services urls

--- src/template/service/CarService.js
+++ src/template/service/CarService.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 const getCarsSmall = () =>
-  axios.get('assets/demo/data/cars-small.json').then(res => res.data.data)
+  axios.get('/assets/demo/data/cars-small.json').then(res => res.data.data)
 
 const getCarsMedium = () =>
-  axios.get('assets/demo/data/cars-medium.json').then(res => res.data.data)
+  axios.get('/assets/demo/data/cars-medium.json').then(res => res.data.data)
 
 const getCarsLarge = () =>
-  axios.get('assets/demo/data/cars-large.json').then(res => res.data.data)
+  axios.get('/assets/demo/data/cars-large.json').then(res => res.data.data)
 
 export default { getCarsSmall, getCarsMedium, getCarsLarge }
--- src/template/service/EventService.js
+++ src/template/service/EventService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const getEvents = () =>
-  axios.get('assets/demo/data/scheduleevents.json').then(res => res.data.data)
+  axios.get('/assets/demo/data/scheduleevents.json').then(res => res.data.data)
 
 export default { getEvents }
